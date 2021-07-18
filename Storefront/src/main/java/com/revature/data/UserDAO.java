ec29a7a8beb97a409fcbcdc48a9b6e509b836331
mcinerneym <mcinerney.michael.t@gmail.com>
2021-07-18 08:08:35 -0700

    Finished tests for current UserServices

--- Storefront/src/main/java/com/revature/data/UserDAO.java
+++ Storefront/src/main/java/com/revature/data/UserDAO.java
@@ -11,94 +11,107 @@ import com.revature.models.AccountType;
 import com.revature.models.User;
 
 public class UserDAO {
-	public static List<User> users; //List of all the users
-	
-	private static String filename = "users.dat"; //Name of the file where the users are stored at.
-	
+	public static List<User> users; // List of all the users
+
+	private static String filename = "users.dat"; // Name of the file where the users are stored at.
+
 	private static final Logger log = LogManager.getLogger(UserDAO.class); // Used to create log
 
-	
 	/**
-	 * static block that loads the file, or populates the array with default accounts if no file exists.
+	 * static block that loads the file, or populates the array with default
+	 * accounts if no file exists.
 	 */
 	static {
 		try {
 			log.trace("UserDAO is getting " + filename);
 			users = new Serializer<User>().readObjectsFromFile(filename);
-		} catch (Exception e) { //Logs the error, the app will continue as usual
+		} catch (Exception e) { // Logs the error, the app will continue as usual
 			log.warn(filename + " was not found.");
 		}
-		
-		if(users == null) { //If users ends up being null
+
+		if (users == null) { // If users ends up being null
 			users = new ArrayList<User>();
-			users.add(new User(users.size(), "DefaultUser", "DefaultPassword", "defaultUser@email.com", AccountType.CUSTOMER, true));
-			users.add(new User(users.size(), "DefaultManager", "DefaultPassword", "defaultManager@email.com", AccountType.MANAGER, true));
-			users.add(new User(users.size(), "admin", "123password@123", "admin@email.com", AccountType.ADMINISTRATOR, true));
+			users.add(new User(users.size(), "DefaultUser", "DefaultPassword", "defaultUser@email.com",
+					AccountType.CUSTOMER, true));
+			users.add(new User(users.size(), "DefaultManager", "DefaultPassword", "defaultManager@email.com",
+					AccountType.MANAGER, true));
+			users.add(new User(users.size(), "admin", "123password@123", "admin@email.com", AccountType.ADMINISTRATOR,
+					true));
 			users.add(new User(users.size(), "badUser", "pass", "bad@user.com", AccountType.CUSTOMER, false));
 			log.debug("Initialized list of default users: " + users);
 		}
 	}
-	
+
 	/**
 	 * Get the user based on the username and password
+	 * 
 	 * @param username The username of the User to get
 	 * @param password The password of the User to get
 	 * @return The User with the same username and password
 	 */
 	public User getUser(String username, String password) {
 		log.trace("App has entered getUser.");
-		log.debug("getUser Parameters: username: " + username+ ", password: " + password);
-		for(User u : users) { //Iterate through each User
-			if (username.equals(u.getUsername()) && password.equals(u.getPassword())) { //If the username and password of a User is the same as the parameters
+		log.debug("getUser Parameters: username: " + username + ", password: " + password);
+		for (User u : users) { // Iterate through each User
+			if (username.equals(u.getUsername()) && password.equals(u.getPassword())) { // If the username and password
+																						// of a User is the same as the
+																						// parameters
 				log.trace("App is now leaving getUser.");
 				log.debug("getUser is returning User: " + u);
-				return u; //Return the correct User
+				return u; // Return the correct User
 			}
 		}
 		log.trace("App is now leaving getUser.");
 		log.debug("getUser is returning User: " + null);
-		return null; //Only returns null if no matching user was found.
+		return null; // Only returns null if no matching user was found.
 	}
-	
+
 	/**
 	 * Save the current list of users to the file
 	 */
 	public void writeToFile() {
 		log.trace("App is now in writeToFile.");
-		new Serializer<User>().writeObjectsToFile(users, filename); //Call the serializer to write to the file
+		new Serializer<User>().writeObjectsToFile(users, filename); // Call the serializer to write to the file
 		log.trace("App is exiting writeToFile.");
 	}
-	
+
 	/**
 	 * Creates a new User and adds it to the list
+	 * 
 	 * @param username The username of the new User
 	 * @param password The password of the new User
-	 * @param email The email of the new User
-	 * @param type The type of account of the new User
+	 * @param email    The email of the new User
+	 * @param type     The type of account of the new User
 	 * @return The new User added to the list
 	 */
 	public User createUser(String username, String password, String email, AccountType type) {
 		log.trace("App has entered createUser.");
-		log.debug("createUser Parameters: username: " + username+ ", password: " + password + ", email: " + email + ", type: "+ type);
-		User newUser = new User(users.size(), username, password, email, type, true); //Create the new user
+		log.debug("createUser Parameters: username: " + username + ", password: " + password + ", email: " + email
+				+ ", type: " + type);
+		if (username == null || username.isBlank() || password == null || password.isBlank() || email == null
+				|| email.isBlank() || type == null) {
+			return null;
+		}
+		User newUser = new User(users.size(), username, password, email, type, true); // Create the new user
 		log.debug("newUser has been created: " + newUser);
-		users.add(newUser); //Add the new user to the list
+		users.add(newUser); // Add the new user to the list
 		log.debug("New user was added to the list: " + users.contains(newUser));
 		log.trace("App is now leaving createUser.");
 		log.debug("createUser is returning User: " + newUser);
 		return newUser;
 	}
-	
+
 	/**
 	 * Check to see if the username has been taken already.
+	 * 
 	 * @param username The username to check
 	 * @return true if the username is unique. false otherwise
 	 */
 	public Boolean checkUsername(String username) {
 		log.trace("App has entered checkUsername.");
 		log.debug("checkUsername Parameters: username: " + username);
-		for (User user : users) { //Iterate through the list of users.
-			if (username.equals(user.getUsername())) { //If the username has been taken
+		for (User user : users) { // Iterate through the list of users.
+			if (username.equals(user.getUsername())) { // If the username has been taken
 				log.debug(username + " has been found: " + user.getUsername());
 				log.trace("App is now leaving checkUsername.");
 				log.debug("checkUsername is returning boolean: " + false);
@@ -107,25 +120,32 @@ public class UserDAO {
 		}
 		log.trace("App is now leaving checkUsername.");
 		log.debug("checkUsername is returning boolean: " + true);
-		return true; //Means the username is unique
+		return true; // Means the username is unique
 	}
-	
+
 	/**
 	 * Searches for a User by username, account type, and active status
+	 * 
 	 * @param searchString The username to search by
-	 * @param type The types of accounts to filter by
-	 * @param status The active status of accounts to filter by
-	 * @return A list of users that contain the username and match the set type and status. An empty List otherwise.
+	 * @param type         The types of accounts to filter by
+	 * @param status       The active status of accounts to filter by
+	 * @return A list of users that contain the username and match the set type and
+	 *         status. An empty List otherwise.
 	 */
-	public List<User> findUsersByName(String searchString, AccountType type, boolean status){
+	public List<User> findUsersByName(String searchString, AccountType type, boolean status) {
 		log.trace("App has entered findUsersByName.");
-		log.debug("findUsersByName Parameters: searchString: " + searchString + ", type: " + type + ", status: " + status);
-		ArrayList<User> retUsers = new ArrayList<User>(); //Initialize the list
-		for (User user : users) { //Iterate through the list of users
-			//If the User username contains the parameter searchString, and has the same type and active status as the parameter type and active status.
-			if (user.getUsername().contains(searchString) && user.getAccountType() == type && user.isActive() == status) {
-				log.debug("Adding User to the list: " + user);
-				retUsers.add(user);
+		log.debug("findUsersByName Parameters: searchString: " + searchString + ", type: " + type + ", status: "
+				+ status);
+		ArrayList<User> retUsers = new ArrayList<User>(); // Initialize the list
+		if (searchString != null && type != null) {
+			for (User user : users) { // Iterate through the list of users
+				// If the User username contains the parameter searchString, and has the same
+				// type and active status as the parameter type and active status.
+				if (user.getUsername().contains(searchString) && user.getAccountType() == type
+						&& user.isActive() == status) {
+					log.debug("Adding User to the list: " + user);
+					retUsers.add(user);
+				}
 			}
 		}
 		log.trace("App is now leaving findUsersByName.");
--- Storefront/src/test/java/com/revature/services/UserServiceTest.java
+++ Storefront/src/test/java/com/revature/services/UserServiceTest.java
@@ -5,14 +5,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
-import org.mockito.internal.util.reflection.FieldSetter;
 
 import com.revature.data.UserDAO;
+import com.revature.models.AccountType;
 import com.revature.models.User;
+import com.revature.util.MockitoHelper;
 
 public class UserServiceTest {
 	private UserService service = null;
@@ -28,7 +31,8 @@ public class UserServiceTest {
 		user.setUsername("Test");
 		user.setPassword("password");
 		user.setEmail("test@test.com");
-
+		user.setAccountType(AccountType.CUSTOMER);
+		user.setActive(true);
 	}
 
 	@Test
@@ -43,16 +47,17 @@ public class UserServiceTest {
 
 	@Test
 	public void testIsUsernameUniqueCallsMethods() {
-		setMockDAO();
+		dao = new MockitoHelper<UserDAO>(UserDAO.class).setPrivateMock(service, "ud");
 		String username = "Test";
 		service.isUsernameUnique(username);
-		
+
 		ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-		Mockito.verify(dao).checkUsername(captor.capture()); // Verifies that the checkUsername function is called from UserDAO.
+		Mockito.verify(dao).checkUsername(captor.capture()); // Verifies that the checkUsername function is called from
+																// UserDAO.
 		Mockito.verify(dao).writeToFile(); // Verifies that the writeToFile function is called from the UserDAO.
-		
+
 		assertEquals(username, captor.getValue(), "Assert that username provided is used.");
-	
+
 	}
 
 	@Test
@@ -61,30 +66,32 @@ public class UserServiceTest {
 		String password = "DefaultPassword";
 		User loggedInTrue = service.login("DefaultUser", "DefaultPassword"); // This is the starting user for
 																				// Storefront.
-		//ArgumentCaptor<String> usernameCaptor = ArgumentCaptor.forClass(String.class);
-		//ArgumentCaptor<String> passwordCaptor = ArgumentCaptor.forClass(String.class);
-		
+		// ArgumentCaptor<String> usernameCaptor =
+		// ArgumentCaptor.forClass(String.class);
+		// ArgumentCaptor<String> passwordCaptor =
+		// ArgumentCaptor.forClass(String.class);
+
 		assertEquals(username, loggedInTrue.getUsername(),
 				"Assert that the username given is the same as the logged in user.");
 		assertEquals(password, loggedInTrue.getPassword(),
 				"Assert that the password given is the same as the logged in user.");
 
 	}
-	
+
 	@Test
 	public void testLoginCallsMethods() {
-		setMockDAO();
+		dao = new MockitoHelper<UserDAO>(UserDAO.class).setPrivateMock(service, "ud");
 		String username = "DefaultUser";
 		String password = "DefaultPassword";
-		
+
 		service.login(username, password);
-		
+
 		ArgumentCaptor<String> usernameCaptor = ArgumentCaptor.forClass(String.class);
 		ArgumentCaptor<String> passwordCaptor = ArgumentCaptor.forClass(String.class);
-		
+
 		Mockito.verify(dao).getUser(usernameCaptor.capture(), passwordCaptor.capture());
-		Mockito.verify(dao).writeToFile(); //Verify that writeToFile is called.
-		
+		Mockito.verify(dao).writeToFile(); // Verify that writeToFile is called.
+
 		assertEquals(username, usernameCaptor.getValue(), "Assert that method called with username given.");
 		assertEquals(password, passwordCaptor.getValue(), "Assert that method called with password given.");
 	}
@@ -92,73 +99,179 @@ public class UserServiceTest {
 	@Test
 	public void testLoginReturnsInvalidUser() {
 		User loggedInFalse = service.login("FakeUser", "FakePass");
-		assertEquals(loggedInFalse, null, "Assert that a bad username and bad password results in a null user being returned.");
+		assertEquals(loggedInFalse, null,
+				"Assert that a bad username and bad password results in a null user being returned.");
 		User loggedInBadUser = service.login("FakeUser", "DefaultPassword");
 		assertEquals(loggedInBadUser, null, "Assert that a bad username results in a null user being returned.");
 		User loggedInBadPass = service.login("DefaultUser", "FakePass");
 		assertEquals(loggedInBadPass, null, "Assert that a bad password results in a null user being returned.");
-		
+
 		User loggedInNull = service.login(null, null);
-		assertEquals(loggedInNull, null, "Assert that a null username and null password results in a null user being returned.");
+		assertEquals(loggedInNull, null,
+				"Assert that a null username and null password results in a null user being returned.");
 		User loggedInNullPass = service.login("DefaultUser", null);
 		assertEquals(loggedInNullPass, null, "Assert that a null password results in a null user being returned.");
 		User loggedInNullUser = service.login(null, "DefaultPassword");
 		assertEquals(loggedInNullUser, null, "Assert that a null username results in a null user being returned.");
 	}
-	
+
 	@Test
 	public void testChangeUserDetails() {
-		setMockDAO();
+		dao = new MockitoHelper<UserDAO>(UserDAO.class).setPrivateMock(service, "ud");
 		User savedUser = service.changeUserDetails(user);
 		User nullUser = service.changeUserDetails(null);
-		
-		Mockito.verify(dao).writeToFile(); //Verifies that writeToFile is called.
-		
+
+		Mockito.verify(dao).writeToFile(); // Verifies that writeToFile is called.
+
 		assertTrue(user.equals(savedUser), "Assert that User passed in is the same User.");
 		assertNull("Assert that null User returns null.", nullUser);
 	}
-	
+
 	@Test
 	public void testRegisterReturnsValidUser() {
-		//TODO: Implement tests for registering valid users
-	}
-	
-	@Test
-	public void testRegisterReturnsInvalidUser() {
-		//TODO: Implement tests for registering invalid users
-	}
-	
-	@Test
-	public void testRegisterReturnsCallsFunctions() {
-		//TODO: Implement tests making sure function calls UserDAO methods
-	}
-	
-	@Test
-	public void testSearchUserByNameReturnsValidUserList() {
-		//TODO: Implement tests for registering valid users
-	}
-	
-	@Test
-	public void testSearchUserByNameReturnsInvalidUserList() {
-		//TODO: Implement tests for registering invalid users
-	}
-	
-	@Test
-	public void testSearchUserByNameReturnsCallsFunctions() {
-		//TODO: Implement tests making sure function calls UserDAO methods
+		// Implement tests for registering valid users
+		String username = "regiserTest";
+		String password = "registerTestPassword";
+		String email = "register@test.com";
+		AccountType type = AccountType.CUSTOMER;
+
+		User newUser = service.register(username, password, email, type);
+
+		assertEquals(username, newUser.getUsername(),
+				"Assert that the username given is the username of the new User.");
+		assertEquals(password, newUser.getPassword(),
+				"Assert that the password given is the password of the new User.");
+		assertEquals(email, newUser.getEmail(), "Assert that the email given is the email of the new User.");
+		assertEquals(type, newUser.getAccountType(),
+				"Assert that the AccountType given is the AccountType of the new User.");
+
+		// These are set for the new user and should the same for all new users.
+		assertTrue(newUser.isActive(), "Assert that the new user isActive is set to true.");
+		assertTrue(newUser.getPastOrders().isEmpty(),
+				"Assert that the new user's past orders list is initialized and empty.");
+		assertTrue(newUser.getCart().isEmpty(), "Assert that the new user's cart is initialized and empty.");
+
 	}
 
-	// Function to set the UserDAO dao to a Mockito mock to verify the functions are
-	// being called.
-	private void setMockDAO() {
-		// Using reflection to get the private dao.
-		dao = Mockito.mock(UserDAO.class);
-		try {
-			FieldSetter.setField(service, service.getClass().getDeclaredField("ud"), dao);
-		} catch (NoSuchFieldException e) {
-			e.printStackTrace();
-		} catch (SecurityException e) {
-			e.printStackTrace();
-		}
+	@Test
+	public void testRegisterReturnsInvalidUser() {
+		// Implement tests for registering invalid users
+		String username = "username";
+		String password = "password";
+		String email = "email";
+		AccountType type = AccountType.CUSTOMER;
+
+		// Null Username
+		User nullUsername = service.register(null, password, email, type);
+		assertNull("Assert that a null username results in a null user.", nullUsername);
+
+		// Blank Username
+		User blankUsername = service.register("   ", password, email, type);
+		assertNull("Assert that a blank username results in a null user.", blankUsername);
+
+		// Null Password
+		User nullPassword = service.register(username, null, email, type);
+		assertNull("Assert that a null password results in a null user.", nullPassword);
+
+		// Blank Password
+		User blankPassword = service.register(username, "      ", email, type);
+		assertNull("Assert that a blank password results in a null user.", blankPassword);
+
+		// Null Email
+		User nullEmail = service.register(username, password, null, type);
+		assertNull("Assert that a null email results in a null user.", nullEmail);
+
+		// Blank Email
+		User blankEmail = service.register(username, password, "", type);
+		assertNull("Assert that a blank email results in a null user.", blankEmail);
+
+		// Null AccountType
+		User nullType = service.register(username, password, email, null);
+		assertNull("Assert that a null AccountType results in a null user.", nullType);
 	}
+
+	@Test
+	public void testRegisterReturnsCallsFunctions() {
+		// Tests making sure method calls UserDAO methods
+		dao = new MockitoHelper<UserDAO>(UserDAO.class).setPrivateMock(service, "ud");
+
+		service.register(user.getUsername(), user.getPassword(), user.getEmail(), user.getAccountType());
+
+		// Set the captors to make sure the methods are called.
+		ArgumentCaptor<String> usernameCaptor = ArgumentCaptor.forClass(String.class);
+		ArgumentCaptor<String> passwordCaptor = ArgumentCaptor.forClass(String.class);
+		ArgumentCaptor<String> emailCaptor = ArgumentCaptor.forClass(String.class);
+		ArgumentCaptor<AccountType> typeCaptor = ArgumentCaptor.forClass(AccountType.class);
+
+		// Verify the methods are called
+		Mockito.verify(dao).createUser(usernameCaptor.capture(), passwordCaptor.capture(), emailCaptor.capture(),
+				typeCaptor.capture());
+		Mockito.verify(dao).writeToFile(); // Verify that writeToFile is called.
+
+		// Verify it is the same values
+		assertEquals(user.getUsername(), usernameCaptor.getValue(),
+				"Assert that createUser called with username given.");
+		assertEquals(user.getPassword(), passwordCaptor.getValue(),
+				"Assert that createUser called with password given.");
+		assertEquals(user.getEmail(), emailCaptor.getValue(), "Assert that createUser called with email given.");
+		assertEquals(user.getAccountType(), typeCaptor.getValue(),
+				"Assert that createUser called with AccountType given.");
+
+	}
+
+	@Test
+	public void testSearchUserByNameReturnsValidUserList() {
+		// Tests for searching for valid usernames, types, and status
+		List<User> userList = service.searchUserByName("DefaultUser", AccountType.CUSTOMER, true);
+
+		assertFalse(userList.isEmpty(), "Assert that the returned user list is not empty.");
+
+		// Different type than the user has
+		List<User> wrongType = service.searchUserByName("DefaultUser", AccountType.MANAGER, true);
+		assertNull("Assert that different AccountType returns a null.", wrongType);
+
+		// Different status than the user has
+		List<User> wrongStatus = service.searchUserByName("DefaultUser", AccountType.CUSTOMER, false);
+		assertNull("Assert that different AccountType returns a null.", wrongStatus);
+	}
+
+	@Test
+	public void testSearchUserByNameReturnsInvalidUserList() {
+		// Tests for making sure invalid parameters returns null
+		// Test for null username
+		List<User> nullUsername = service.searchUserByName(null, user.getAccountType(), user.isActive());
+		assertNull("Assert that null username returns null.", nullUsername);
+
+		// Test for null AccountType
+		List<User> nullType = service.searchUserByName(user.getUsername(), null, user.isActive());
+		assertNull("Assert that null AccountType returns null.", nullType);
+
+	}
+
+	@Test
+	public void testSearchUserByNameReturnsCallsFunctions() {
+		// Tests making sure method calls UserDAO methods
+		dao = new MockitoHelper<UserDAO>(UserDAO.class).setPrivateMock(service, "ud");
+
+		service.searchUserByName(user.getUsername(), user.getAccountType(), user.isActive());
+
+		// Set the captors to make sure the methods are called.
+		ArgumentCaptor<String> usernameCaptor = ArgumentCaptor.forClass(String.class);
+		ArgumentCaptor<AccountType> typeCaptor = ArgumentCaptor.forClass(AccountType.class);
+		ArgumentCaptor<Boolean> statusCaptor = ArgumentCaptor.forClass(Boolean.class);
+
+		// Verify the methods are called
+		Mockito.verify(dao).findUsersByName(usernameCaptor.capture(), typeCaptor.capture(), statusCaptor.capture());
+		Mockito.verify(dao).writeToFile(); // Verify that writeToFile is called.
+
+		// Verify it is the same values
+		assertEquals(user.getUsername(), usernameCaptor.getValue(),
+				"Assert that findUsersByName called with username given.");
+		assertEquals(user.getAccountType(), typeCaptor.getValue(),
+				"Assert that findUsersByName called with AccountType given.");
+		assertEquals(user.isActive(), statusCaptor.getValue(),
+				"Assert that findUsersByName called with AccountType given.");
+
+	}
+
 }
